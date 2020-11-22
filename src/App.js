0f21af99c3b486eb2546f0084e515e4fd54c63d7
Franciszek Pindel <franciszek.pin@gmail.com>
2020-11-22 21:20:21 +0100

    Change project file structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
+import React from 'react';
 import './App.scss';
-import './layoutBrowser/LayoutBrowser'
-import LayoutBrowser from "./layoutBrowser/LayoutBrowser";
+import LayoutPreview from "./layout-preview/LayoutPreview";
+import {Component} from "react";
+import MostlyFluid from "./layout-preview/layout-patterns/MostlyFluid";
+import ColumnDrop from "./layout-preview/layout-patterns/ColumnDrop";
+import LayoutStructure from "./layout-preview/layout-patterns/layoutStructure";
+import Controls from "./controls/Controls";
+import TinyTweaks from "./layout-preview/layout-patterns/TinyTweaks";
+import LayoutDescription from "./layout-description/LayoutDescription";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {layoutNumber: 0, viewerWidth: 800};
+        this.numberOfLayouts = 3;
+        this.layoutTitles = ["MostlyFluid", "ColumnDrop", "TinyTweaks"];
+        this.layouts = [<MostlyFluid/>, <ColumnDrop/>, <TinyTweaks/>];
+         }
 
 
-function App() {
-    return (
-        <div className="App">
-            <LayoutBrowser />
-        </div>
-    );
+    incrementLayoutNumber = () => {
+        this.setState(
+            state =>
+                ({layoutNumber: (state.layoutNumber + 1) % this.numberOfLayouts})
+        );
+    }
+
+    decrementLayoutNumber = () => {
+        this.setState(
+            state =>
+                ({layoutNumber: (state.layoutNumber - 1 + this.numberOfLayouts) % this.numberOfLayouts})
+        );
+    }
+
+    updateViewerWidth = (x) => {
+        this.setState({viewerWidth: x});
+    }
+
+    getUpdatedLayoutToShow = () => {
+        const layoutStructure = <LayoutStructure viewerWidth={this.state.viewerWidth}/>
+        let actualLayoutToShow = this.layouts[this.state.layoutNumber];
+        return React.cloneElement(actualLayoutToShow, {layoutStructure: layoutStructure})
+    }
+
+    getUpdatedTitleName = () => {
+        return this.layoutTitles[this.state.layoutNumber];
+    }
+
+
+    render() {
+        const actualLayoutToShow = this.getUpdatedLayoutToShow();
+        const actualTitleToShow = this.getUpdatedTitleName();
+
+        return (
+            <div className="LayoutBrowser">
+                <header>
+                    <div className="Header">
+                        <p>Responsive Layout Pattern Viewer</p>
+                        <p className="title">{actualTitleToShow}</p>
+                    </div>
+                </header>
+
+                <LayoutPreview layoutToShow={actualLayoutToShow} width={this.state.viewerWidth}/>
+
+                <Controls
+                    incrementLayoutNumber={this.incrementLayoutNumber}
+                    decrementLayoutNumber={this.decrementLayoutNumber}
+                    updateViewerWidth={this.updateViewerWidth}
+                />
+
+                <LayoutDescription layoutNumber={this.state.layoutNumber} />
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,7 +1,7 @@
 import './Controls.scss'
-import LayoutSwitcherLeft from "./layout-switcher/LayoutSwitcherLeft";
-import LayoutSwitcherRight from "./layout-switcher/LayoutSwitcherRight";
-import ResizeLayout from "./resize-layout-slider/ResizeLayout";
+import LayoutSwitcherLeft from "../layout-switcher/LayoutSwitcherLeft";
+import LayoutSwitcherRight from "../layout-switcher/LayoutSwitcherRight";
+import ResizeLayout from "../resize-layout-slider/ResizeLayout";
 import React from "react";
 
 function Controls(props) {
